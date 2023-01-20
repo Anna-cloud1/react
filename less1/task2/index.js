dfbaa92e7b006a692cac0f32beda05fde05998a8
Anna Chufarova <anyatrying@gmail.com>
2023-01-20 22:13:40 +0200

    gg

--- less1/task2/index.js
+++ less1/task2/index.js
@@ -21,7 +21,7 @@ const rootElement = document.querySelector('#root');
 
 
 const element = React.createElement('div', { className: 'greeting' }, [
-  React.createElement('div', { className: 'greeting__title' }, 'Hello, World!'),
+  React.createElement('div', { className: 'greeting__title' }, 'Hello, world!'),
   React.createElement('div', { className: 'greeting__text' }, `I'm learning React`),
 ]);
 
