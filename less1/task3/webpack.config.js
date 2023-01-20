eb8f94f36aa7010ab2daca41c1f1a3ccc470531b
Anna Chufarova <anyatrying@gmail.com>
2023-01-21 01:21:20 +0200

    ph

---
+++ less1/task3/webpack.config.js
@@ -0,0 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
