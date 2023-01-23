48fd581c43bd51d594382cddf9ee9585bd17816a
Anna Chufarova <anyatrying@gmail.com>
2023-01-23 14:51:44 +0200

    ph

---
+++ less3/task5/src/Profile.jsx
@@ -0,0 +1,19 @@
+import './index.scss';
+import React from 'react';
+import moment from 'moment';
+
+// const birthDate = moment(new Date(userData.birthDate)).format('DD MMM YY');
+
+const Profile = props => {
+  const birthDate = moment(new Date(props.userData.birthDate)).format('DD MMM YY');
+  return (
+    <div className="profile">
+      <div className="profile__name">
+        {props.userData.firstName} {props.userData.lastName}
+      </div>
+      <div className="profile__birth">{`Was born in ${birthDate}, ${props.userData.birthPlace}`}</div>
+    </div>
+  );
+};
+
+export default Profile;
