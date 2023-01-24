87e7ca7762595e5108d885717df373a6c50cd24a
Anna Chufarova <anyatrying@gmail.com>
2023-01-25 00:29:23 +0200

    ph

---
+++ less5/task2/src/GoodButton.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './index.scss'
+
+
+class GoodButton extends React.Component {
+  handleClick(e) {
+    // console.log(e.target)
+    alert('Good job!')
+
+  }
+  render() {
+    return (
+      <button className="fancy-button" 
+      onClick={this.handleClick}>
+        Click me!
+      </button>
+    );
+  }
+}
+
+
+export default GoodButton;
