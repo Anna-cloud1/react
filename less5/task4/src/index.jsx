c39967f8f39efb3879fc1c7464c275ccf69a14d7
Anna Chufarova <anyatrying@gmail.com>
2023-01-25 21:27:47 +0200

    ph

---
+++ less5/task4/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Colors from './Colors';
+
+const rootElement = document.querySelector('#root');
+const element = <Colors />;
+
+ReactDOM.render(element, rootElement);
