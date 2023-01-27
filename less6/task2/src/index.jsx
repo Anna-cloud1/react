1b921a13dc1bf582149ea22df0bcadab22265088
Anna Chufarova <anyatrying@gmail.com>
2023-01-27 22:30:54 +0200

    ph

--- less6/task2/src/index.jsx
+++ less6/task2/src/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import './index.scss';
+import Mailbox from './Mailbox'
 
 const rootElement = document.querySelector('#root');
-const element = < />;
+const element = < Mailbox unreadMessages={['aaa', 4334, 'g']} />;
 
 ReactDOM.render(element, rootElement);
+
+
