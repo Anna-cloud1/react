efe903793f0af8140cf95a90fa55a18fc389114e
Anna Chufarova <anyatrying@gmail.com>
2023-01-28 00:42:27 +0200

    ph

--- less6/task3/src/Page.jsx
+++ less6/task3/src/Page.jsx
@@ -5,34 +5,27 @@ const text1 = 'Price is 500$. Available in 2 colors';
 const text2 = 'Price is 650$. Not available';
 
 class Page extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      text: null,
+      info: null,
     };
   }
 
-  setText = text => {
+  setText = info => {
     this.setState({
-      text,
+      info,
     });
   };
   render() {
     return (
       <div className="page">
-        <Info text={this.state.text} />
+        <Info info={this.state.info} />
         <div className="actions">
-          <button
-            className="btn"
-            onClick={() => this.setText(text1)}
-          >
+          <button className="btn" onClick={() => this.setText(text1)}>
             IPhone 13
           </button>
-          <button
-            className="btn"
-            onClick={() => this.setText(text2)}
-          >
+          <button className="btn" onClick={() => this.setText(text2)}>
             IPhone 13 Pro
           </button>
           <button className="btn" onClick={() => this.setText(null)}>
