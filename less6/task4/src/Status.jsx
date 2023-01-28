deb75639e7c353a01f914a21d270acffb55bdef8
Anna Chufarova <anyatrying@gmail.com>
2023-01-28 11:57:10 +0200

    ph

--- less6/task4/src/Status.jsx
+++ less6/task4/src/Status.jsx
@@ -5,12 +5,12 @@ import Offline from './Offline';
 class Status extends Component {
   constructor(props) {
     super(props);
-    // this.state = {
-    //   isOnline,
-    // };
+    this.state = {
+      isOnline: props.isOnline,
+    };
   }
   render() {
-    return <div className="status">{this.props.isOnline ? <Offline /> : <Online />}</div>;
+    return <div className="status">{this.props.isOnline ? <Online /> : <Offline />}</div>;
   }
 }
 
