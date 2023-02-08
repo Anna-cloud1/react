daf014b03140c72b3b3f77b010edf0cb2d7859e8
Anna Chufarova <anyatrying@gmail.com>
2023-02-08 22:56:27 +0200

    ph

--- less8/task1/src/index.jsx
+++ less8/task1/src/index.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles.scss';
-import Demo from './Demo'
+import './index.scss';
 
 const rootElement = document.querySelector('#root');
 
 
-ReactDOM.render(< Demo />, rootElement);
+ReactDOM.render(, rootElement);
---
+++ less8/task2/src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Clock from './Clock';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <button onClick>Toggle</button>
+      </div>
+    );
+  }
+}
+
+export default App;
