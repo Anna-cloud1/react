6fb993fecbae723d3ccc99885ab64f250060930b
Anna Chufarova <anyatrying@gmail.com>
2023-02-08 23:53:53 +0200

    ph

--- less8/task2/src/App.jsx
+++ less8/task2/src/App.jsx
@@ -2,10 +2,30 @@ import React, { Component } from 'react';
 import Clock from './Clock';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: true,
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      visible: !this.state.visible,
+    });
+  }
+
   render() {
     return (
       <div>
-        <button onClick>Toggle</button>
+        <button className="btn" onClick={this.toggle}>
+          Toggle
+        </button>
+        {this.state.visible && <Clock location="Argentina" offset={3} />}
+        {this.state.visible && <Clock location="Kyiv" offset={2} />}
+        {this.state.visible && <Clock location="Madrid" offset={1} />}
       </div>
     );
   }
--- less8/task2/src/Clock.jsx
+++ less8/task2/src/Clock.jsx
@@ -1,24 +1,75 @@
+// import React, { Component } from 'react';
+
+// class Clock extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       date: new Date(),
+//     };
+//   }
+
+//   componentDidMount() {
+//     this.interval = setInterval(() => {
+//       this.setState({
+//         date: new Date(),
+//       });
+//     }, 1000);
+//   }
+
+//   componentWillUnmount() {
+//     clearInterval(this.interval);
+//   }
+
+//   render() {
+//     return (
+//       <div className="clock">
+//         <div className="clock__location">{this.location}</div>
+//         <div className="clock__time">{this.state.date.toLocaleTimeString()}</div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Clock;
+
+
 import React, { Component } from 'react';
+import moment from 'moment';
+
+const getTimeWithOffset = offset => {
+  const currentTime = new Date();
+  const utcOffset = currentTime.getTimezoneOffset() / 60;
+  return new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset));
+};
 
 class Clock extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      date: new Date(),
+      offset: moment(getTimeWithOffset(this.props.offset)).format('LTS'),
     };
   }
 
   componentDidMount() {
-
-        setInterval(() => {
-          this.setState({
-            date: new Date(),
-          });
-        }, 1000);
+    this.interval = setInterval(() => {
+      this.setState({
+        offset: moment(getTimeWithOffset(this.props.offset)).format('LTS'),
+      });
+    }, 1000);
   }
 
+    componentWillUnmount() {
+      clearInterval(this.interval);
+    }
+
   render() {
-    return <div>{this.state.date.toLocaleTimeString()}</div>;
+    return (
+      <div className="clock">
+        <div className="clock__location">{this.props.location}</div>
+        <div className="clock__time">{this.state.offset}</div>
+      </div>
+    );
   }
 }
 
