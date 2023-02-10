88d6e2c4cde7a684caadacd7741fce51ffd5a1a9
Anna Chufarova <anyatrying@gmail.com>
2023-02-10 18:29:31 +0200

    ph

--- less8/task3/src/Dimensions.jsx
+++ less8/task3/src/Dimensions.jsx
@@ -6,19 +6,6 @@ class Dimension extends Component {
     height: null,
   };
 
-
-  onResize = e => {
-    const { innerWidth, innerHeight } = e.target;
-    this.setDimensions(innerHeight, innerWidth);
-  };
-  setDimensions = (width, height) => {
-    this.setState({
-      width,
-      height,
-    });
-    document.title = `${innerWidth} x ${innerHeight}`;
-  };
-
   componentDidMount() {
     window.addEventListener('resize', this.onResize);
     const { innerWidth, innerHeight } = window;
@@ -28,6 +15,32 @@ class Dimension extends Component {
   componentWillUnmount() {
     window.removeEventListener('resize', this.onResize);
   }
+
+  onResize = e => {
+    const { innerWidth, innerHeight } = e.target;
+    this.setDimensions(innerWidth, innerHeight);
+  };
+
+  setDimensions = (width, height) => {
+    this.setState({
+      width,
+      height,
+    });
+    document.title = `${width} x ${height}`;
+  };
+  onResize = e => {
+    const { innerWidth, innerHeight } = e.target;
+    this.setDimensions(innerWidth, innerHeight);
+  };
+
+  setDimensions = (width, height) => {
+    this.setState({
+      width,
+      height,
+    });
+    document.title = `${width} x ${height}`;
+  };
+
   render() {
     return <div className="dimensions">{`${this.state.width}px - ${this.state.height}px`}</div>;
   }
