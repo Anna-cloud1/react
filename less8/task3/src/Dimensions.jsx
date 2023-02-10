360912d7fd0543d4c44e4164180c6d81836a0bcc
Anna Chufarova <anyatrying@gmail.com>
2023-02-10 18:06:05 +0200

    ph

--- less8/task3/src/Dimensions.jsx
+++ less8/task3/src/Dimensions.jsx
@@ -1,21 +1,11 @@
 import React, { Component } from 'react';
 
-class Dimensions extends Component {
+class Dimension extends Component {
   state = {
     width: null,
     height: null,
   };
 
-
-  componentDidMount() {
-    window.addEventListener('resize', this.onResize);
-    const { innerWidth, innerHeight } = window;
-    this.setDimensions(innerWidth, innerHeight)
-  }
-
-  componentWillUnmount() {
-    window.addEventListener('resize', this.onResize);
-  }
 
   onResize = e => {
     const { innerWidth, innerHeight } = e.target;
@@ -29,9 +19,18 @@ class Dimensions extends Component {
     document.title = `${innerWidth} x ${innerHeight}`;
   };
 
+  componentDidMount() {
+    window.addEventListener('resize', this.onResize);
+    const { innerWidth, innerHeight } = window;
+    this.setDimensions(innerWidth, innerHeight);
+  }
+
+  componentWillUnmount() {
+    window.addEventListener('resize', this.onResize);
+  }
   render() {
     return <div className="dimensions">{`${this.state.width}px - ${this.state.height}px`}</div>;
   }
 }
 
-export default Dimensions;
+export default Dimension;
