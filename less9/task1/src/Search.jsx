84b58f8bcbcc55987902080aebd998246e321990
Anna Chufarova <anyatrying@gmail.com>
2023-02-12 23:54:00 +0200

    ph??

--- less9/task1/src/Search.jsx
+++ less9/task1/src/Search.jsx
@@ -3,28 +3,24 @@ import React, { Component } from 'react';
 class Search extends Component {
   state = {
     value: '',
-  };
+  }
 
-  handleChange = event => {
-    this.setState({ value: event.target.value });
-  };
+  handleChange = (event) => {
+this.setState({value:event.target.value})
+  }
 
-  search = event => {
+  search = (event) => {
     event.preventDefault();
     alert(`Search text: ${this.state.value}`);
-  };
-  render() {
+  }
+  render() { 
     return (
-      <form className="search">
-        <input
-          type="text"
-          onChange={this.handleChange}
-          value={this.state.value}
-          className="search__input"
-        />
-        <button className="search__button" onClick={this.search}>
-          Search
-        </button>
+      <form className="search" onSubmit={this.search}>
+        <input 
+        type="text" 
+        onChange={this.handleChange} 
+        value={this.state.value} className="search__input" />
+        <button className="search__button" type='submit'>Search</button>
       </form>
     );
   }
