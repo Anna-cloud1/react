b4289ce2a943a4a06af8b57a00bdfe8346bc209a
Anna Chufarova <anyatrying@gmail.com>
2023-03-22 00:20:45 +0200

    ph

--- less11/task1/src/index.jsx
+++ less11/task1/src/index.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+import App from './App'
 
 
 const rootElement = document.querySelector('#root');
 
 
-ReactDOM.render(</>, rootElement);
+ReactDOM.render(<App/>, rootElement);
+
+
+{/* <div class="app">
+  <!-- OddNumbers -->
+  <div class="number">
+    <span class="number__title">Odd number</span>
+    <span class="number__value">83</span>
+  </div>
+  <!-- EvenNumbers -->
+  <div class="number">
+    <span class="number__title">Even number</span>
+    <span class="number__value">82</span>
+  </div>
+  <!-- Numbers -->
+  <div class="number">
+    <span class="number__title">All numbers</span>
+    <span class="number__value">83</span>
+  </div>
+  <!-- Numbers -->
+  <div class="number">
+    <span class="number__title">Just 17</span>
+    <span class="number__value">17</span>
+  </div>
+</div> */}
