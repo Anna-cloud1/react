22b55baaeb0feffc5fe3e24b7799743ad78a5287
Anna Chufarova <anyatrying@gmail.com>
2023-03-29 01:54:26 +0300

    ph

--- less12/todo-list/src/CreateTaskInput.jsx
+++ less12/todo-list/src/CreateTaskInput.jsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const CreateTaskInput = () => {
-  return (
-    <div className="create-task">
-      <input type="text" className="create-task__input" />
-      <button className="btn">Create</button>
-    </div>
-  );
-};
+class CreateTaskInput extends Component {
+  state = {
+    value: '',
+  };
+
+  handleChange = event => {
+    this.setState({
+      value: event.target.value,
+    });
+  };
+
+  handleTaskCreate = () => {
+    this.props.onCreate(this.state.value);
+    this.setState({ value: '' });
+  };
+
+  render() {
+    return (
+      <div className="create-task">
+        <input
+          type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
+          className="create-task__input"
+        />
+        <button className="btn" onClick={this.handleTaskCreate}>
+          Create
+        </button>
+      </div>
+    );
+  }
+}
 
 export default CreateTaskInput;
+
+// 1 взять текст из инпута +
+// 2 создать ззадание с этим тестом (создать объект с айди, тесктом и чекбоксом) +
+// 3 добавить объект в весь список +
--- less12/todo-list/src/TasksList.jsx
+++ less12/todo-list/src/TasksList.jsx
@@ -2,24 +2,110 @@ import React, { Component } from 'react';
 import CreateTaskInput from './CreateTaskInput';
 import Task from './Task';
 
+const baseUrl = `https://6422fd57001cb9fc20354c18.mockapi.io/api/v1/tasks`;
+
 class TasksList extends Component {
   state = {
-    tasks: [
-      { text: 'Buy milk', done: false, id: 1 },
-      { text: 'Pick up Tom from airport', done: false, id: 2 },
-      { text: 'Visit party', done: false, id: 3 },
-      { text: 'Visit doctor', done: true, id: 4 },
-      { text: 'Buy meat', done: true, id: 5 },
-    ],
+    tasks: [],
+  };
+
+  componentDidMount() {
+    this.fetchTasksList();
+  }
+
+  fetchTasksList = () => {
+    fetch(baseUrl)
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        }
+      })
+      .then(tasksList => {
+        this.setState({
+          tasks: tasksList,
+        });
+      });
+  };
+
+  onCreate = text => {
+    // для создания на сервере
+    // 1 create task object +
+    // 2. post object to server +
+    // 3.  fetch list from server +
+
+    const newTask = {
+      text,
+      done: false,
+    };
+
+    fetch(baseUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;utc-8',
+      },
+      body: JSON.stringify(newTask),
+    }).then(response => {
+      if (response.ok) {
+        this.fetchTasksList();
+      } else {
+        throw new Error('Failed to create task');
+      }
+    });
+
+    //   const updatedTasks = tasks.concat(newTask);
+    //   this.setState({
+    //     tasks: updatedTasks,
+    //   });
+  };
+
+  handleTaskStatusChange = id => {
+    //1 find task in a list
+    //2 toggle done value
+    //3 save updt list
+
+    const updatedTasks = this.state.tasks.map(task => {
+      if (task.id === id) {
+        return {
+          ...task,
+          done: !task.done,
+        };
+      }
+      return task;
+    });
+    this.setState({
+      tasks: updatedTasks,
+    });
+  };
+
+  handleTaskDelete = id => {
+    // 1 filter tasks
+    // 2. update state
+
+    fetch(`${baseUrl}/${id}`, {
+      method: 'DELETE',
+    }).then(response => {
+      if (response.ok) {
+        this.fetchTasksList();
+      } else {
+        throw new Error('Failed to delete task');
+      }
+    });
   };
 
   render() {
+    const sortedList = this.state.tasks.slice().sort((a, b) => a.done - b.done);
+
     return (
       <div className="todo-list">
-        < CreateTaskInput /> 
+        <CreateTaskInput onCreate={this.onCreate} />
         <ul className="list">
-          {this.state.tasks.map(task => (
-            <Task key={task.id} {...task} />
+          {sortedList.map(task => (
+            <Task
+              key={task.id}
+              {...task}
+              onChange={this.handleTaskStatusChange}
+              onDelete={this.handleTaskDelete}
+            />
           ))}
         </ul>
       </div>
