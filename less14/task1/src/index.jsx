54347b4d3648850e20cda9daa4c33ba2b6007d7a
Anna Chufarova <anyatrying@gmail.com>
2023-04-02 23:36:11 +0300

    ph

--- less14/task1/src/index.jsx
+++ less14/task1/src/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Counter from './Counter';
 import './index.scss';
 
 
 const rootElement = document.querySelector('#root');
 
 
-ReactDOM.render(</>, rootElement);
+ReactDOM.render(< Counter/>, rootElement);
