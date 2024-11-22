6368a5a3b5fbd0605cb70a48e868146aeed14032
Anna Chufarova <anyatrying@gmail.com>
2024-11-22 10:23:00 +0200

    Добавляем

--- less8/task1/src/index.jsx
+++ less8/task1/src/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Demo from './Demo';
 import './index.scss';
 
 const rootElement = document.querySelector('#root');
 
 
-ReactDOM.render(, rootElement);
+ReactDOM.render(<Demo/>, rootElement);
